40e87df21c52fb2e70bd1c7b4e78f658d2e5d9fd
Jimmy Nguyen <nphumy89@gmail.com>
2019-05-31 06:06:43 +1200

    add StudentBean

--- StudentServer/src/java/BeanPackage/Student.java
+++ StudentServer/src/java/BeanPackage/Student.java
@@ -10,7 +10,71 @@ package BeanPackage;
  * @author MY PHU NGUYEN
  */
 public class Student {
+    private static int studentNumber=0;
     private int studentID;
     private String studentName;
     private int age;
+    private Gender gender;
+    
+    public Student(int studentID, String studentName, int age, Gender gender)
+    {
+        this.studentID= studentID;
+        if (studentID>studentNumber)
+        {
+            studentNumber= studentID;
+        }
+        this.studentName= studentName;
+        this.age=age;
+        this.gender=gender;
+    }
+    
+    public Student (String studentName, int age, Gender gender)
+    {
+        this.studentName= studentName;
+        this.age=age;
+        this.gender=gender;
+        studentNumber++;
+        this.studentID=studentNumber;
+    }
+    
+    //getter and setter
+    public static int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public static void setStudentNumber(int studentNumber) {
+        Student.studentNumber = studentNumber;
+    }
+    
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+    
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ StudentServer/src/java/BeanPackage/StudentBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeanPackage;
+
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author MY PHU NGUYEN
+ */
+@Stateless
+public class StudentBean {
+    private ArrayList<Student> studentList;
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public Student createStudent(String name)
+    {
+        Student aStudent = new Student(name,18, Gender.Male);
+        return aStudent;
+    }
+    
+    public int plusStudent(String name)
+    {
+        Student aStudent = new Student(name,18, Gender.Male);
+        studentList.add(aStudent);
+        return studentList.size();
+    }
+    
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+}
