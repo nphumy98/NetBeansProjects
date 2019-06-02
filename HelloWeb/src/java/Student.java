0df4870167181961650642460f6441959e931e4e
Jimmy Nguyen <nphumy89@gmail.com>
2019-06-03 07:43:08 +1200

    testing on jaxb

---
+++ HelloWeb/src/java/Student.java
@@ -0,0 +1,118 @@
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MY PHU NGUYEN
+ */
+
+/**
+ *
+ * @author MY PHU NGUYEN
+ */
+@XmlRootElement
+public class Student {
+    private static int studentNumber=0;
+    private int studentID;
+    private String studentName;
+    private int age;
+    private Gender gender;
+    private String password;
+    
+    public Student ()
+    {
+        super();
+    }
+    
+    public Student(int studentID, String studentName, int age, Gender gender, String password)
+    {
+        this.studentID= studentID;
+        if (studentID>studentNumber)
+        {
+            studentNumber= studentID;
+        }
+        this.studentName= studentName;
+        this.age=age;
+        this.gender=gender;
+        this.password=password;
+    }
+    
+    public Student (String studentName, int age, Gender gender)
+    {
+        this.studentName= studentName;
+        this.age=age;
+        this.gender=gender;
+        studentNumber++;
+        this.studentID=studentNumber;
+        this.password="123";
+    }
+    
+    //getter and setter
+    public static int getStudentNumber() {
+        return studentNumber;
+    }
+    
+    public static void setStudentNumber(int studentNumber) {
+        Student.studentNumber = studentNumber;
+    }
+    
+    @XmlElement
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+    
+    public Gender getGender() {
+        return gender;
+    }
+    
+    @XmlElement
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    
+    public String getStudentName() {
+        return studentName;
+    }
+    
+    @XmlElement
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    @XmlElement
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    @XmlAttribute
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    public int getStudentID() {
+        return studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "studentID=" + studentID + ", studentName=" + studentName + ", age=" + age + ", gender=" + gender + ", password=" + password + '}';
+    }
+    
+    
+    
+}
+
---
+++ HelloWeb/src/java/test.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MY PHU NGUYEN
+ */
+import java.io.File;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.bind.JAXB;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+
+public class test {
+   public static void main(String[] args) throws JAXBException {
+      //Create a student object
+      Student student = new Student("Minh",18, Gender.Male);
+
+      //fill details of the student
+
+ 
+         //create JAXBElement of type Student
+         //Pass it the student object
+        JAXBElement<Student> jaxbElement =  new JAXBElement( 
+            new QName(Student.class.getSimpleName()), Student.class, student);
+
+         //Create a String writer object which will be 
+         //used to write jaxbElment XML to string
+         StringWriter writer = new StringWriter();
+
+         // create JAXBContext which will be used to update writer 		
+         JAXBContext context = JAXBContext.newInstance(Student.class);
+
+         // marshall or convert jaxbElement containing student to xml format
+         context.createMarshaller().marshal(jaxbElement, writer);
+          System.out.println( writer.toString());
+         StringReader sr = new StringReader(writer.toString());
+         
+         Unmarshaller unmarshall = context.createUnmarshaller();
+         Student aStudent=    JAXB.unmarshal(sr, Student.class);
+         
+         System.out.println(aStudent);
+      
+   }
+}
